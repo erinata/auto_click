4c38ff34e599dc04a152642038e9a718c9398f28
BrianPurgert <brianpurgert2@gmail.com>
2023-11-24 02:02:59 -0500

    1

--- lib/auto_click/user32.rb
+++ lib/auto_click/user32.rb
@@ -1,9 +1,9 @@
 module User32
-	extend Fiddle::Importer
-	dlload 'user32'
-	extern "int GetCursorPos(char*)"
-	extern "int SetCursorPos(int,int)"
-	extern "int SendInput(int,char*,int)"
-	extern "int GetKeyState(int)"
-	extern "int GetSystemMetrics(int)"
+    extend Fiddle::Importer
+    dlload 'user32'
+    extern "int GetCursorPos(char*)"
+    extern "int SetCursorPos(int,int)"
+    extern "int SendInput(int,char*,int)"
+    extern "int GetKeyState(int)"
+    extern "int GetSystemMetrics(int)"
 end
